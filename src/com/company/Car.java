f79f64d5b5c401ed56602aacb1d1f0224dd6b80c
Halasek <jakubha2000@gmail.com>
2021-10-02 09:28:57 +0200

    Zadanie 1

---
+++ src/com/company/Car.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Car {
+    String producer;
+    String model;
+    Double millage;
+    Double engineSize;
+    Integer yearOfProduction;
+    String color;
+
+
+}
