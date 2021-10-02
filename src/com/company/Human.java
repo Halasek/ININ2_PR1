5fef1648966e1740cdfb10e5a735526221f21918
Halasek <jakubha2000@gmail.com>
2021-10-02 10:05:39 +0200

    Zadanie 3 i 4

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -1,10 +1,35 @@
 package com.company;
 
+import java.util.Date;
+
 public class Human {
     String firstName;
     String lastName;
     Integer age;
-    Double salary;
+    private Double salary;
     Animal pet;
     Car car;
+
+    Human() {
+        this.salary = 1200.0;
+    }
+
+    void setSalary(Double salary) {
+        if (salary > 0) {
+            System.out.println("Dane zostały wysłane do systemu księgowego");
+            System.out.println("Koniecnie odbierz aneks do umowy od pani Hani");
+            System.out.println("ZUS i US już wiedzą o zmianie wypłaty i nie ma sensu ukrywać");
+            this.salary = salary;
+        }else{
+            System.out.println("Wypłata: " + this.salary + "Wypłata nie może być ujemna");
+
+        }
+    }
+
+    Double getSalary() {
+        Date dateandTime = new Date();
+        System.out.println(dateandTime);
+        System.out.println("wynagrodzenie: " + this.salary);
+        return this.salary;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -46,5 +46,6 @@ public class Main {
         System.out.println("I have " + me.car.producer + " " + me.car.model);
         System.out.println("waga psa: " + dog.weight);
 
+        me.getSalary();
     }
 }
